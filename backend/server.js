eb3cd45f9bd36e9ec09f9c63d3c01a69407dd77f
Abhigyan200724 <kushwahachotu243@gmail.com>
2026-06-13 10:36:36 +0530

    Enhance CORS settings in server.js
    Updated CORS configuration to allow requests from the specified origin.

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,11 @@ const app = express();
 
 
 
-app.use(cors());
+app.use(cors({
+     origin: "https://sdg-web-app.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/user",userRoutes);
