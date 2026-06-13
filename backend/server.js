fb9f99fa0ef932c2ffc630cb19dc1500a23dff46
Abhigyan200724 <kushwahachotu243@gmail.com>
2026-06-13 15:28:53 +0530

    Refactor server.js to simplify CORS configuration

--- backend/server.js
+++ backend/server.js
@@ -16,11 +16,7 @@ const app = express();
 
 
 
-app.use(cors({
-     origin: "https://sdg-web-app.vercel.app",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/user",userRoutes);
